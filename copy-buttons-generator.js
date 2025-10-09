d66106cdbdec200d4ba52b9a8185e6180e6ed852
blue marlin <alexhou@duck.com>
2025-10-09 11:48:33 +1100

    Fix scope

--- copy-buttons-generator.js
+++ copy-buttons-generator.js
@@ -11,69 +11,72 @@
 (function() {
     'use strict';
 
-    console.log('[Remote Script (01)]: Loaded');
+    console.log('[Remote Script (01)] Loaded');
 
-    /**
-     * Creates copy buttons wrapped in a HTML div with given product ID and name values, at a given font size
-     * Requires Google Fonts' Material Symbols
-     * 
-     * @param {string} idValue - Product ID
-     * @param {string} nameValue - Product name
-     * @param {string} fontSize - Button font size
-     * @returns {HTMLElement} wrapper div containing both copy buttons
-     */
-    remoteScript01.createCopyButtonsWrapper = function(idValue, nameValue, fontSize) {
-        console.log('[Remote Script (01)]: createCopyButtonsWrapper called');
+    window.remoteScript = {
 
-        const nameBtn = document.createElement('span');
+        /**
+         * Creates copy buttons wrapped in a HTML div with given product ID and name values, at a given font size
+         * Requires Google Fonts' Material Symbols
+         * 
+         * @param {string} idValue - Product ID
+         * @param {string} nameValue - Product name
+         * @param {string} fontSize - Button font size
+         * @returns {HTMLElement} wrapper div containing both copy buttons
+         */
+        createCopyButtonsWrapper(idValue, nameValue, fontSize) {
+            console.log('[Remote Script (01)] createCopyButtonsWrapper called');
 
-        nameBtn.className = 'material-symbols-outlined';
-        nameBtn.title = 'Copy product name';
-        nameBtn.textContent = 'content_copy';
-        //nameBtn.style.background = 'white';
-        nameBtn.style.color = 'rgba(145, 145, 145, 0.4)';
-        nameBtn.style.cursor = 'pointer';
-        if (typeof fontSize !== 'undefined') nameBtn.style.fontSize = fontSize;
-        nameBtn.style.zIndex = '99';
+            const nameBtn = document.createElement('span');
 
-        nameBtn.addEventListener('click', () => {
-            navigator.clipboard.writeText(nameValue)
-                .then(() => {
-                nameBtn.textContent = 'check_small';
-                setTimeout(() => nameBtn.textContent = 'content_copy', 1500);
+            nameBtn.className = 'material-symbols-outlined';
+            nameBtn.title = 'Copy product name';
+            nameBtn.textContent = 'content_copy';
+            //nameBtn.style.background = 'white';
+            nameBtn.style.color = 'rgba(145, 145, 145, 0.4)';
+            nameBtn.style.cursor = 'pointer';
+            if (typeof fontSize !== 'undefined') nameBtn.style.fontSize = fontSize;
+            nameBtn.style.zIndex = '99';
+
+            nameBtn.addEventListener('click', () => {
+                navigator.clipboard.writeText(nameValue)
+                    .then(() => {
+                    nameBtn.textContent = 'check_small';
+                    setTimeout(() => nameBtn.textContent = 'content_copy', 1500);
+                });
             });
-        });
 
-        const idBtn = nameBtn.cloneNode(true);
+            const idBtn = nameBtn.cloneNode(true);
 
-        idBtn.title = 'Copy product ID';
-        idBtn.textContent = 'pin';
+            idBtn.title = 'Copy product ID';
+            idBtn.textContent = 'pin';
 
-        idBtn.addEventListener('click', () => {
-            navigator.clipboard.writeText(idValue)
-                .then(() => {
-                idBtn.textContent = 'check_small';
-                setTimeout(() => idBtn.textContent = 'pin', 1500);
+            idBtn.addEventListener('click', () => {
+                navigator.clipboard.writeText(idValue)
+                    .then(() => {
+                    idBtn.textContent = 'check_small';
+                    setTimeout(() => idBtn.textContent = 'pin', 1500);
+                });
             });
-        });
 
-        const wrapper = document.createElement('div');
-        wrapper.style.display = 'inline-flex';
-        wrapper.appendChild(idBtn);
-        wrapper.appendChild(nameBtn);
+            const wrapper = document.createElement('div');
+            wrapper.style.display = 'inline-flex';
+            wrapper.appendChild(idBtn);
+            wrapper.appendChild(nameBtn);
 
-        return wrapper;
-    }
+            return wrapper;
+        },
 
-    /**
-     * Add Google's Material Symbols content_copy icon request to document head tag
-     */
-    remoteScript01.loadGoogleFontsIcons = function() {
-        console.log('[Remote Script (01)]: loadGoogleFontsIcons called');
+        /**
+         * Add Google's Material Symbols content_copy icon request to document head tag
+         */
+        loadGoogleFontsIcons() {
+            console.log('[Remote Script (01)] loadGoogleFontsIcons called');
 
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&icon_names=content_copy'; //:opsz,wght,FILL,GRAD@20..48,400,0,0';
-        document.head.appendChild(link); // error checking needed?
-    }
+            const link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&icon_names=content_copy'; //:opsz,wght,FILL,GRAD@20..48,400,0,0';
+            document.head.appendChild(link); // error checking needed?
+        }
+    };
 })();
