dfd1b14494b308a9cef7983bdd61f342e94cef65
blue marlin <alexhou@duck.com>
2025-10-16 12:58:28 +1100

    Replace loadScript with await fetch

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         (Fetch) Script loading
+// @name         (Fetcher) Script loading
 // @namespace    https://github.com/blue-marIin/
 // @version      1.0
 // @description  Load private scripts
@@ -14,14 +14,17 @@
     window.Fetcher = {
         scriptTag: '[Fetcher]',
 
-        loadScript(url) {
-            return new Promise((resolve, reject) => {
-                const el = document.createElement('script');
-                //el.src = url + '?_=' + Date.now(); // cache buster
-                el.onload = () => resolve(url);
-                el.onerror = reject;
-                document.head.appendChild(el);
-            });
+        async loadScript(url) {
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                return data
+            } catch (error) {
+                log.error('Fetch error:', error);
+            }
         }
     }
 })();
