3371ed1c31e7e9f9140a71d3da985b5810f9dca8
blue marlin <alexhou@duck.com>
2025-12-05 13:23:18 +1100

    feat!: add hypertext button & misc changes
    - Update function docs & internal commenting
    - G Fonts import link now dynamically constructed from IconTypes object
    - Check for presence of G Fonts import link before constructing & appending
    - Extract button order values into constant
    - Miscellaneous variable renaming
    - Increment version: 1.0 -> 2.0
    
    breaking change: createCopyButton now takes string as input for button
    type instead of IconType

--- copy-buttons.js
+++ copy-buttons.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         (CopyButtons) Copy buttons generator
 // @namespace    https://github.com/blue-marIin/
-// @version      1.0
+// @version      2.0
 // @description  Helper function(s) for local scripts inserting product copy buttons
 // @author       BLUE MARLIN
 // @match        -
@@ -14,37 +14,55 @@
     const CB_CONSTANTS = {
         buttonCheckTimeout: 1500,
 
-        IconType: {
+        IconMap: {
             CHECK: 'check_small',
-            ID: 'pin',
-            NAME: 'content_copy'
+            NAME: 'content_copy',
+            HYPER: 'link',
+            ID: 'pin'
+        },
+
+        buttonOrder: {
+            id: 0,
+            name: 1,
+            hyper: 2
         }
     };
 
     const googleFontsImportLink = `
-        https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&icon_names=${CB_CONSTANTS.IconType.CHECK},${CB_CONSTANTS.IconType.NAME},${CB_CONSTANTS.IconType.ID}
-    `;
+        https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined&icon_names=${Object.values(CB_CONSTANTS.IconMap).sort().join()}
+    `; // icon_names must be in comma-separated alphabetical order or else import breaks
 
     const CB_BASE_STYLE = {
         idButton: {
             className: "material-symbols-outlined",
-            textContent: CB_CONSTANTS.IconType.ID,
+            textContent: CB_CONSTANTS.IconMap.ID,
             title: "Copy product ID",
             style: {
                 cursor: "pointer",
                 fontSize: "inherit",
-                order: "0"
+                order: CB_CONSTANTS.buttonOrder.id
             }
         },
 
         nameButton: {
             className: "material-symbols-outlined",
-            textContent: CB_CONSTANTS.IconType.NAME,
+            textContent: CB_CONSTANTS.IconMap.NAME,
             title: "Copy product name",
             style: {
                 cursor: "pointer",
                 fontSize: "inherit",
-                order: "1"
+                order: CB_CONSTANTS.buttonOrder.name
+            }
+        },
+
+        hyperButton: {
+            className: "material-symbols-outlined",
+            textContent: CB_CONSTANTS.IconMap.HYPER,
+            title: "Copy hyperlinked product name", // may need to change this
+            style: {
+                cursor: "pointer",
+                fontSize: "inherit",
+                order: CB_CONSTANTS.buttonOrder.hyper
             }
         },
 
@@ -58,60 +76,57 @@
     // ===== MAIN =====
     window.CopyButtons = {
         scriptTag: '[CopyButtons]',
-        CONSTANTS: CB_CONSTANTS,
 
         /**
-         * Creates copy buttons wrapped in a HTML div with given product ID and name values, at a given font size
-         * Requires Google Fonts' Material Symbols
-         * 
-         * @param {object} productData - { id: '12345', name: 'Product name' } - values to be copied
-         * @param {Partial<CSSStyleDeclaration>} additionalDivWrapperStyling - Tweak style further for different insertion contexts
-         *      eg: { fontSize: '12px', gap: '1px', top '30%' }
-         * @returns {HTMLElement} wrapper div containing both copy buttons
+         * Create hyperlink ClipboardItem from given URL and text
+         * @param {string} url - Hyperlink URL
+         * @param {string} text - Display text (generally product name)
+         * @returns {ClipboardItem}
          */
-        createCopyButtonsWrapper(productData, additionalDivWrapperStyling) {
-            console.log(`${this.scriptTag} createCopyButtonsWrapper called`); // Keep basic console.log for remote scripts
+        createClipboardItem(url, text) {
+            const clipboardItem = new ClipboardItem({
+                'text/html': new Blob( // Hypertext
+                    [`<a href="${url}">${text}</a>`],
+                    { type: 'text/html' }
+                ),
+                'text/plain': new Blob( // Plaintext fallback - 'Shampoo 500mL: https://shamp-store.com/product/shampoo-500ml'
+                    [`${text}: ${url}`],
+                    { type: 'text/plain' }
+                )
+            });
 
-            const idButton = this.createCopyButton(CB_CONSTANTS.IconType.ID, productData.id);
-            const nameButton = this.createCopyButton(CB_CONSTANTS.IconType.NAME, productData.name);
-
-            const wrapper = document.createElement('div');
-
-            Object.assign(wrapper.style, CB_BASE_STYLE.divWrapper);
-            if (additionalDivWrapperStyling && typeof additionalDivWrapperStyling === 'object') { Object.assign(wrapper.style, additionalDivWrapperStyling); }
-
-            wrapper.appendChild(idButton);
-            wrapper.appendChild(nameButton);
-
-            return wrapper;
+            return clipboardItem;
         },
 
         /**
          * Create button element and assign value, styling and listener
-         * @param {CB_CONSTANTS.IconType} buttonType - either CONSTANTS.IconType.NAME or CONSTANTS.IconType.ID
-         * @param {string} buttonCopyData - Value to be copied
-         * @param {Partial<CSSStyleDeclaration>} additionalButtonStyling - Tweak button styling further
+         * @param {string} type - Type of button to be created. One of: [name, id, hyper]
+         * @param {string} data - Value to be copied
+         * @param {Partial<CSSStyleDeclaration>} extraStyling - Tweak button styling further
          *      eg: { verticalAlign: 'text-top' }
          * @returns {HTMLButtonElement}
          */
-        createCopyButton(buttonType, buttonCopyData, additionalButtonStyling) {
+        createCopyButton(type, data, extraStyling) {
+            const buttonConfig = CB_BASE_STYLE[`${type}Button`];
+
+            // Create button and assign HTML attributes
             const button = document.createElement('span');
-            const styleConfig = (buttonType === CB_CONSTANTS.IconType.ID)
-                ? CB_BASE_STYLE.idButton
-                : CB_BASE_STYLE.nameButton;
+            Object.assign(button, {
+                className: buttonConfig.className,
+                textContent: buttonConfig.textContent,
+                title: buttonConfig.title
+            });
 
-            button.className = styleConfig.className;
-            button.textContent = styleConfig.textContent;
-            button.title = styleConfig.title;
+            // Assign base CSS style and if it was passed, assign extra styling
+            Object.assign(button.style, buttonConfig.style);
+            if (extraStyling && typeof extraStyling === 'object') { Object.assign(button.style, extraStyling); }
 
-            Object.assign(button.style, styleConfig.style);
-            if (additionalButtonStyling && typeof additionalButtonStyling === 'object') { Object.assign(button.style, additionalButtonStyling); }
-
+            // On click: copy data to clipboard, set icon to small check & set timeout to reset icon
             button.addEventListener('click', () => {
-                navigator.clipboard.writeText(buttonCopyData)
+                navigator.clipboard.writeText(data)
                     .then(() => {
-                        button.textContent = CB_CONSTANTS.IconType.CHECK;
-                        setTimeout(() => button.textContent = buttonType, CB_CONSTANTS.buttonCheckTimeout);
+                        button.textContent = CB_CONSTANTS.IconMap.CHECK;
+                        setTimeout(() => button.textContent = buttonConfig.textContent, CB_CONSTANTS.buttonCheckTimeout);
                     });
             });
 
@@ -119,9 +134,43 @@
         },
 
         /**
-         * Add Google's Material Symbols content_copy & pin icons request to document head tag
+         * Creates copy buttons wrapped in a HTML div with given product ID and name values, at a given font size
+         * Requires Google Fonts' Material Symbols
+         *
+         * @param {object} productData - { id: '12345', name: 'Product name', hyper: {ClipboardItem} }
+         *      Button types to be created, and data to be copied.
+         * @param {Partial<CSSStyleDeclaration>} extraDivStyling - Tweak style further for different insertion contexts
+         *      eg: { fontSize: '12px', gap: '1px', top '30%' }
+         * @returns {HTMLElement} wrapper div containing both copy buttons
+         */
+        createCopyButtonsWrapper(productData, extraDivStyling) {
+            console.log(`${this.scriptTag} createCopyButtonsWrapper called`); // Keep basic console.log for remote scripts
+
+            const wrapper = document.createElement('div');
+
+            // Assign base CSS style and if it was passed, assign extra styling
+            Object.assign(wrapper.style, CB_BASE_STYLE.divWrapper);
+            if (extraDivStyling && typeof extraDivStyling === 'object') { Object.assign(wrapper.style, extraDivStyling); }
+
+            // Iterate through productData and create buttons for each valid item
+            for (const [type, data] of Object.entries(productData)) {
+                // Only proceed if productData key is a valid button type ['id', 'name', 'hyper']
+                if (Object.keys(CB_CONSTANTS.buttonOrder).includes(type)) {
+                    let btn = this.createCopyButton(type, data);
+                    wrapper.appendChild(btn);
+                } else {
+                    console.warn('Unable to create button. Invalid button type:', type, data);
+                }
+            }
+
+            return wrapper;
+        },
+
+        /**
+         * Add Google's Material Symbols content_copy, check_small, pin and link icons request to document head tag
          */
         loadGoogleFontsIcons() {
+            if (document.querySelector(`link[href="${googleFontsImportLink.trim()}"]`)) return;
             console.log(`${this.scriptTag} loadGoogleFontsIcons called`);
 
             const link = document.createElement('link');
